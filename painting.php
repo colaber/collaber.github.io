9934a4ad0695949344f9fcd6445aafd61892da7a
colaber <kirillfootballer@yandex.ru>
2022-01-25 01:36:14 +0300

    Add files via upload

---
+++ painting.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Живопись / Художник Николай Климов</title>
+  <meta name="robots" content="index,follow,noodp">
+  <meta property="og:locale" content="ru_RU">
+  <link rel="apple-touch-icon" sizes="57x57" href="/icons/icon57x57.png" data-mce-href="/icons/icon57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="/icons/icon60x60.png" data-mce-href="/icons/icon60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="/icons/icon72x72.png" data-mce-href="/icons/icon72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="/icons/icon76x76.png" data-mce-href="/icons/icon76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="/icons/icon114x114.png" data-mce-href="/icons/icon114x140.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="/icons/icon120x120.png" data-mce-href="/icons/icon120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="/icons/icon144x144.png" data-mce-href="/icons/icon144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="/icons/icon152x152.png" data-mce-href="/icons/icon152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="/icons/icon180x180.png" data-mce-href="/icons/icon180x180.png">
+  <link rel="icon" type="image/png" href="/icons/icon32x32.png" sizes="32x32" data-mce-href="/icons/icon32x32.png">
+  <link rel="icon" type="image/png" href="/icons/icon192x192.png" sizes="192x192" data-mce-href="/icons/icon192x192.png">
+  <link rel="icon" type="image/png" href="/icons/icon96x96.png" sizes="96x96" data-mce-href="/icons/icon96x96.png">
+  <link rel="icon" type="image/png" href="/icons/icon16x16.png" sizes="16x16" data-mce-href="/icons/icon16x16.png">
+  <meta name="msapplication-TileColor" content="#151515">
+  <meta name="theme-color" content="#151515">
+  <meta name="msapplication-navbutton-color" content="#151515">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+  <meta name="viewport" content="ya-title=#151515,ya-dock=fade">
+  <link rel="stylesheet" href="general.css">
+</head>
+<body>
+  <div class="desktop" style="background-image: none;">
+    <div class="page_bg"><div id="l"></div><div id="r"></div></div>
+    <div class="l_block">
+      <div id="s_title">художник</div>
+      <div id="b_title">Николай<br>Климов</div>
+      <hr>
+      <div class="a_pages">
+        <p>Живопись</p>
+        <a href="/icon">Икона</a><br>
+        <a href="/bio">Биография</a>
+      </div>
+      <div class="bottom">
+        <a href="/" id="buy_button">Купить работу</a>
+        <a href="tg://resolve?domain=klimovkir" id="telegram"></a>
+        <a href="https://wa.me/79175564100" id="whatsapp"></a>
+      </div>
+    </div>
+    <div class="r_block">
+      <div id="viewer"></div>
+      <div class="works">
+        <div id="pic" style="background-image:url('img/bg.png');"></div>
+        <div id="pic" style="background-image:url('img/bg.png');"></div>
+        <div id="pic" style="background-image:url('img/bg.png');"></div>
+        <div id="pic" style="background-image:url('img/bg.png');"></div>
+        <div id="pic" style="background-image:url('img/bg.png');"></div>
+        <div id="pic" style="background-image:url('img/bg.png');"></div>
+        <div id="pic" style="background-image:url('img/bg.png');"></div>
+        <div id="pic" style="background-image:url('img/bg.png');"></div>
+        <div id="pic" style="background-image:url('img/bg.png');"></div>
+        <div id="pic" style="background-image:url('img/bg.png');"></div>
+        <div id="pic" style="background-image:url('img/bg.png');"></div>
+        <div id="pic" style="background-image:url('img/bg.png');"></div>
+      </div>
+      <p id="credits" style="color:#151515;">Все права защищены &#169; 2021 Николай Климов</p>
+    </div>
+  </div>
+  <div class="mobile">
+    <div class="top">
+      <div class="title">
+        <div id="s_title"></div>
+        <div id="b_title"></div>
+      </div>
+      <div id="menu_btn"></div>
+    </div>
+    <div class="mcontent">
+    </div>
+  </div>
+</body>
+</html>
